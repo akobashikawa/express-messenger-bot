54f3b6086392e55b9be58809a78c5ae0e4017a8f
Rulo Kobashikawa <akobashikawa@gmail.com>
2019-07-31 12:08:03 -0500

    Agrega soporte para testing de endpoints

--- test/index-rt.js
+++ test/index-rt.js
@@ -1,10 +1,29 @@
 const { describe, Try } = require('riteway');
+const request = require('supertest');
+const app = require('../app');
 
-describe('trivial', async assert => {
-    assert({
-        given: 'hello',
-        should: 'hello',
-        actual: 'hello',
-        expected: 'hello'
-    });
+// describe('trivial', async assert => {
+//     assert({
+//         given: 'hello',
+//         should: 'hello',
+//         actual: 'hello',
+//         expected: 'hello'
+//     });
+// });
+
+describe('/hello', async assert => {
+    const url = '/hello';
+    request(app)
+        .get(url)
+        .expect(200)
+        .end((err, res) => {
+            if (err) throw err;
+
+            return assert({
+                given: 'hello',
+                should: 'return text hello',
+                actual: res.text,
+                expected: 'hello'
+            });
+        });
 });
